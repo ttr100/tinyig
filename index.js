2dc40e304b1bb45296d2f53b7b47bce4dee0c167
Hendra <hendra2392@gmail.com>
2021-10-24 14:44:07 +0700

    html

--- index.js
+++ index.js
@@ -7,8 +7,29 @@ app.use(cookieParser());
 
 const port = 3000;
 
+
+const fs = require('fs');
+const path = require('path');
+
+function html(fileName){
+  let targetFile = path.join('html', fileName);
+  let exist = fs.existsSync(targetFile)
+  if(exist){
+    let content = fs.readFileSync(targetFile, 'utf-8');
+    return content
+  }
+
+  return `File not found ${fileName}`;
+}
+
 app.get('/', function(req, res){
-  res.send('Hello');
+  let response = html('index.html')
+  res.send(response);
+})
+
+app.get('/register', function(req, res){
+  let response = html('register.html')
+  res.send(response);
 })
 
 app.listen(port, () => {
