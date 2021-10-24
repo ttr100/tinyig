f323649b24d1eb387948b26d0709ced204b530d6
Hendra <hendra2392@gmail.com>
2021-10-24 15:39:12 +0700

    Handlebar

--- index.js
+++ index.js
@@ -10,30 +10,27 @@ const port = 3000;
 
 const fs = require('fs');
 const path = require('path');
+const handlebars = require("handlebars");
 
 function html(fileName, data){
   let targetFile = path.join('html', fileName);
   let exist = fs.existsSync(targetFile)
   if(exist){
     let content = fs.readFileSync(targetFile, 'utf-8');
-    let keys = Object.keys(data);
-    for(let i=0;i<keys.length;i++){
-      let currentKey = keys[i]; // name
-      let replacementKey = `#${currentKey}#` // #name#
-      let value = data[currentKey]; // 'Alice'
-      content = content.replace(replacementKey, value);
-    }
-    return content
+    const template = handlebars.compile(content);
+    return template(data);
   }
 
   return `File not found ${fileName}`;
 }
 
 app.get('/', function(req, res){
+  let followers = ['alice', 'bob', 'charlie'].join(',');
   let data = {
     name: 'Bob',
     followerCount: 20,
-    followingCount: 20
+    followingCount: 20,
+    followers: followers
   }
   let response = html('index.html', data)
   res.send(response);
