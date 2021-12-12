9739f2247f87e20f720d5b9b80a85553bdf6a9d0
Hendra <hendra2392@gmail.com>
2021-12-12 14:50:31 +0700

    Add uploads file store

--- uploads.js
+++ uploads.js
@@ -1,5 +1,25 @@
+const fs = require('fs');
 
+let FILE_NAME = 'uploads.txt';
 let images = [];
+
+function storeToFile(data){
+  let objectToStore = {
+    uploads: data
+  }
+
+  let stringToStore = JSON.stringify(objectToStore);
+  fs.writeFileSync(FILE_NAME, stringToStore);
+}
+
+function readFromFile(){
+  if(fs.existsSync(FILE_NAME)){
+    let fileContent = fs.readFileSync(FILE_NAME, 'utf-8');
+    let parsedData = JSON.parse(fileContent);
+    images = parsedData.uploads;
+  }
+}
+
 
 function createNewUpload(username, filePath, originalFileName, caption){
   let newUpload = {
@@ -9,11 +29,14 @@ function createNewUpload(username, filePath, originalFileName, caption){
     caption: caption
   };
   images.push(newUpload);
+  storeToFile(images);
 }
 
 function getUploadedFiles(username){
   return images;
 }
+
+readFromFile();
 
 module.exports = {
   createNewUpload: createNewUpload,
